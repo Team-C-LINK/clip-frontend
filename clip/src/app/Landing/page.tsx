594b2f96567ae9703f10fdaacf672e1579bfb68f
jsl0149 <jsl0149@naver.com>
2024-03-21 20:50:41 +0900

    test : 반응형 웹 테스트 코드 추가

--- clip/src/app/Landing/page.tsx
+++ clip/src/app/Landing/page.tsx
@@ -10,31 +10,55 @@ import LandingFourth from '../../../public/image/LandingFourth.svg';
 import LandingFifth from '../../../public/image/LandingFifth.svg';
 import Sixth from './components/Sixth/Sixth';
 import styled from 'styled-components';
-const LandingPage = () => {
+import { useMediaQuery } from 'react-responsive';
+
+const Landing = () => {
+  return (
+    <>
+      <LandingPageMobile></LandingPageMobile>
+    </>
+  );
+};
+
+const LandingPageMobile = () => {
+  const isMobile = useMediaQuery({
+    query: '(max-width:500px)',
+  });
+
   const moveToChannel = () => {
     window.location.href = 'https://pf.kakao.com/_uRgmG';
   };
 
   return (
     <>
-      <Join onClick={moveToChannel} src={addChannel.src}></Join>
-      <Layout>
-        <LandingPageFrame>
-          <img src={LandingFirst.src}></img>
-          <img src={LandingSecond.src}></img>
-          <img src={LandingThird.src}></img>
-          <img src={LandingFourth.src}></img>
-          <img src={LandingFifth.src}></img>
-          <Sixth></Sixth>
-        </LandingPageFrame>
-      </Layout>
+      <>
+        <Join onClick={moveToChannel} src={addChannel.src}></Join>
+        <Layout>
+          <LandingPageFrame>
+            <img src={LandingFirst.src}></img>
+            <img src={LandingSecond.src}></img>
+            <img src={LandingThird.src}></img>
+            <img src={LandingFourth.src}></img>
+            <img src={LandingFifth.src}></img>
+            <Sixth></Sixth>
+          </LandingPageFrame>
+        </Layout>
+      </>
     </>
   );
 };
 
+const LandingPageWeb = () => {
+  const isWeb = useMediaQuery({
+    query: '(min-width:501px)',
+  });
+
+  return <>{isWeb && <>웹 입니다</>}</>;
+};
+
 const Join = styled.img`
   position: fixed;
-  top: -1rem;
+  bottom: -0.3dvh;
   left: 50%;
   transform: translateX(-50%);
 
@@ -42,4 +66,4 @@ const Join = styled.img`
   background: url(${(props) => props.src});
 `;
 
-export default LandingPage;
+export default Landing;
--- clip/src/app/page.tsx
+++ clip/src/app/page.tsx
@@ -1,3 +1,69 @@
-export default function Home() {
-  return <>Hello, Next.js</>;
-}
+'use client';
+
+import Layout from './components/Background';
+import { Frame, LandingPageFrame } from './Landing/components/Frame';
+import addChannel from '../../public/image/addChannel.svg';
+import LandingFirst from '../../public/image/LandingFirst.svg';
+import LandingSecond from '../../public/image/LandingSecond.svg';
+import LandingThird from '../../public/image/LandingThird.svg';
+import LandingFourth from '../../public/image/LandingFourth.svg';
+import LandingFifth from '../../public/image/LandingFifth.svg';
+import Sixth from './Landing/components/Sixth/Sixth';
+import styled from 'styled-components';
+import { useMediaQuery } from 'react-responsive';
+
+const Landing = () => {
+  return (
+    <>
+      <LandingPageMobile></LandingPageMobile>
+    </>
+  );
+};
+
+const LandingPageMobile = () => {
+  const isMobile = useMediaQuery({
+    query: '(max-width:500px)',
+  });
+
+  const moveToChannel = () => {
+    window.location.href = 'https://pf.kakao.com/_uRgmG';
+  };
+
+  return (
+    <>
+      <>
+        <Join onClick={moveToChannel} src={addChannel.src}></Join>
+        <Layout>
+          <LandingPageFrame>
+            <img src={LandingFirst.src}></img>
+            <img src={LandingSecond.src}></img>
+            <img src={LandingThird.src}></img>
+            <img src={LandingFourth.src}></img>
+            <img src={LandingFifth.src}></img>
+            <Sixth></Sixth>
+          </LandingPageFrame>
+        </Layout>
+      </>
+    </>
+  );
+};
+
+const LandingPageWeb = () => {
+  const isWeb = useMediaQuery({
+    query: '(min-width:501px)',
+  });
+
+  return <>{isWeb && <>웹 입니다</>}</>;
+};
+
+const Join = styled.img`
+  position: fixed;
+  bottom: -0.3dvh;
+  left: 50%;
+  transform: translateX(-50%);
+
+  z-index: 100;
+  background: url(${(props) => props.src});
+`;
+
+export default Landing;
