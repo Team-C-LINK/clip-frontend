bc58c074644734796ea4bbed585db4ac8b8e5c28
jsl0149 <jsl0149@naver.com>
2024-03-22 22:02:03 +0900

    feat : 로그인 페이지 퍼블리싱

--- clip/src/app/components/Background.tsx
+++ clip/src/app/components/Background.tsx
@@ -1,22 +1,24 @@
+'use client';
+
 import styled from 'styled-components';
+import { useVh } from '../utils/useVh';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const vh = useVh();
+
   return (
     <>
-      <Background>{children}</Background>
+      <Background value={vh}>{children}</Background>
     </>
   );
 };
 
-const Background = styled.div`
-  position: absolute;
-  width: 36rem;
-  height: fit-content;
-  left: 50%;
-  transform: translateX(-50%);
-  &::-webkit-scrollbar {
-    display: none;
-  }
+const Background = styled.body`
+  position: relative;
+  width: 100dvw;
+  height: ${(props) => 100 * props.value}px;
+  font-size: 62.5%;
+  line-height: 1.285;
 `;
 
 export default Layout;
