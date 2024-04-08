da146f5b915e5ba06be1e28e2426c404a287c062
jsl0149 <jsl0149@naver.com>
2024-04-09 00:48:45 +0900

    feat : 내 지원 목록 퍼블리싱

--- clip/src/app/mypage/applications/component/C.style.tsx
+++ clip/src/app/mypage/applications/component/C.style.tsx
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
+export const wrap = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`;
+
 export const filter_wrap = styled.div`
   position: relative;
   display: flex;
@@ -11,7 +19,7 @@ export const filter_wrap = styled.div`
 
   background: #ffffff;
   /* Line/01 */
-  border-bottom: 1px solid #d9d9d9;
+  border-bottom: 0.1rem solid #d9d9d9;
 `;
 export const filter_inner = styled.div`
   position: relative;
@@ -34,7 +42,7 @@ export const filter_status_wrap = styled.div`
   height: 2.6rem;
 `;
 
-export const filter_text = styled.span`
+export const filter_text_selected = styled.span`
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -58,6 +66,24 @@ export const filter_text = styled.span`
   background-color: rgba(37, 37, 37, 0.08);
 `;
 
+export const filter_text_unselected = styled.span`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0.1rem 0.5rem;
+  width: fit-content;
+  height: 2.6rem;
+
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.4rem;
+  line-height: 2rem;
+
+  color: #6c6c6c;
+`;
+
 export const filter_recruiting_wrap = styled.div`
   position: relative;
   display: flex;
@@ -72,4 +98,161 @@ export const filter_recruiting_text = styled.span`
   font-weight: 500;
   font-size: 1.4rem;
   line-height: 2rem;
+`;
+
+export const list_wrap = styled.div`
+  position: relative;
+  display: row;
+  flex-direction: column;
+  flex-wrap: wrap;
+  overflow-y: auto;
+  width: 100%;
+  flex: 1;
+`;
+
+export const list_content_wrap = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: 15rem;
+  width: 100%;
+  border-bottom: 0.1rem solid #d9d9d9;
+`;
+
+export const list_content_wrap_inner = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 91.1dvw;
+  height: fit-content;
+`;
+
+export const list_content_recruit_wrap = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: fit-content;
+  gap: 1.3rem;
+  align-items: center;
+`;
+
+export const list_content_recruit_state = styled.span`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  background-color: #07a320;
+
+  width: 4rem;
+  height: 2.2rem;
+
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1.2rem;
+  line-height: 1.6rem;
+  background-color: rgba(0, 191, 64, 0.08);
+  border-radius: 0.4rem;
+  letter-spacing: 0.03em;
+  color: #00bf40;
+`;
+
+export const list_content_recruit_deadline = styled.span`
+  width: fit-content;
+  height: fit-content;
+
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.2rem;
+  line-height: 1.6rem;
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.03em;
+
+  color: #6c6c6c;
+`;
+
+export const list_content_title = styled.span`
+  width: fit-content;
+  height: fit-content;
+
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.4rem;
+  line-height: 2rem;
+
+  color: #252525;
+`;
+
+export const list_content_condition = styled.span`
+  width: fit-content;
+  height: fit-content;
+
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.2rem;
+  line-height: 1.6rem;
+
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.03em;
+
+  color: #6c6c6c;
+`;
+
+export const list_content_reward_wrap = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: fit-content;
+  gap: 1rem;
+  align-items: center;
+`;
+
+export const list_content_reward = styled.span`
+  width: fit-content;
+  height: fit-content;
+
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.6rem;
+  line-height: 2.4rem;
+
+  color: #252525;
+`;
+
+export const list_content_middle_wrap = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 0.7rem;
+  width: fit-content;
+  height: fit-content;
+`;
+
+export const list_content_condition_wrap = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  height: fit-content;
+`;
+
+export const list_content_interested = styled.img`
+  position: absolute;
+  width: fit-content;
+  height: fit-content;
+  right: 0rem;
+  background: url(${(props) => props.src});
 `;
--- clip/src/app/mypage/applications/page.tsx
+++ clip/src/app/mypage/applications/page.tsx
@@ -1,35 +1,124 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import Layout from '@/app/SharedComponent/Background';
 import Header from '@/app/SharedComponent/Header/Header';
 import * as C from './component/C.style';
 import checkedbox from './asset/checkedBox.svg';
 import uncheckedbox from './asset/uncheckedBox.svg';
+import interested from './asset/interested.svg';
+import uninterested from './asset/uninterested.svg';
 import Image from 'next/image';
 
 const Applications = () => {
+  const [filterState, setFilterState] = useState<string>('신청 완료');
+  const [isRecruiting, setIsRecruiting] = useState<boolean>(false);
+
+  const handleFilterState = (e: any) => {
+    const target = e.target.innerHTML;
+    setFilterState(target);
+  };
+
+  const handleIsRecruiting = () => {
+    if (isRecruiting) setIsRecruiting(false);
+    else setIsRecruiting(true);
+  };
+
+  useEffect(() => {
+    // 필터 상태가 바뀌었을 떄 리스트 갱신 로직 들어가야함
+  }, [filterState]);
+
+  useEffect(() => {
+    // 모집 중 상태가 변경 되었을 때 리스트 갱신 로직
+  }, [isRecruiting]);
+
   return (
     <>
       <Layout>
-        <Header text={'내 지원목록'}></Header>
-        <C.filter_wrap>
-          <C.filter_inner>
-            <C.filter_status_wrap>
-              <C.filter_text>신청 완료</C.filter_text>
-              <C.filter_text>신청 취소</C.filter_text>
-              <C.filter_text>매칭 완료</C.filter_text>
-            </C.filter_status_wrap>
-            <C.filter_recruiting_wrap>
-              <Image
-                src={uncheckedbox.src}
-                alt="uncheckedbox"
-                width={24}
-                height={24}
-              ></Image>
-              <C.filter_recruiting_text>모집중</C.filter_recruiting_text>
-            </C.filter_recruiting_wrap>
-          </C.filter_inner>
-        </C.filter_wrap>
+        <C.wrap>
+          <Header text={'내 지원목록'}></Header>
+          <C.filter_wrap>
+            <C.filter_inner>
+              <C.filter_status_wrap>
+                {filterState === '신청 완료' ? (
+                  <C.filter_text_selected>신청 완료</C.filter_text_selected>
+                ) : (
+                  <C.filter_text_unselected onClick={handleFilterState}>
+                    신청 완료
+                  </C.filter_text_unselected>
+                )}
+                {filterState === '신청 취소' ? (
+                  <C.filter_text_selected>신청 취소</C.filter_text_selected>
+                ) : (
+                  <C.filter_text_unselected onClick={handleFilterState}>
+                    신청 취소
+                  </C.filter_text_unselected>
+                )}
+                {filterState === '매칭 완료' ? (
+                  <C.filter_text_selected>매칭 완료</C.filter_text_selected>
+                ) : (
+                  <C.filter_text_unselected onClick={handleFilterState}>
+                    매칭 완료
+                  </C.filter_text_unselected>
+                )}
+              </C.filter_status_wrap>
+              <C.filter_recruiting_wrap>
+                {isRecruiting ? (
+                  <Image
+                    src={checkedbox.src}
+                    alt="checkedbox"
+                    width={24}
+                    height={24}
+                    onClick={handleIsRecruiting}
+                  ></Image>
+                ) : (
+                  <Image
+                    src={uncheckedbox.src}
+                    alt="uncheckedbox"
+                    width={24}
+                    height={24}
+                    onClick={handleIsRecruiting}
+                  ></Image>
+                )}
+                <C.filter_recruiting_text>모집중</C.filter_recruiting_text>
+              </C.filter_recruiting_wrap>
+            </C.filter_inner>
+          </C.filter_wrap>
+          <C.list_wrap>
+            <C.list_content_wrap>
+              <C.list_content_wrap_inner>
+                <C.list_content_recruit_wrap>
+                  <C.list_content_recruit_state>
+                    모집중
+                  </C.list_content_recruit_state>
+                  <C.list_content_recruit_deadline>
+                    2024.04.31 까지
+                  </C.list_content_recruit_deadline>
+                </C.list_content_recruit_wrap>
+                <C.list_content_middle_wrap>
+                  <C.list_content_title>
+                    배고픔을 느끼는지 알아보는 실험 참여자를 모집합니다.
+                  </C.list_content_title>
+                  <C.list_content_condition_wrap>
+                    <C.list_content_condition>
+                      대상 : 20~30대 남녀
+                    </C.list_content_condition>
+                    <C.list_content_condition>
+                      장소 : 서울시 강남구 강남대로
+                    </C.list_content_condition>
+                  </C.list_content_condition_wrap>
+                </C.list_content_middle_wrap>
+                <C.list_content_reward_wrap>
+                  <C.list_content_condition>사례비</C.list_content_condition>
+                  <C.list_content_reward>60,000원</C.list_content_reward>
+                  <C.list_content_interested
+                    src={interested.src}
+                  ></C.list_content_interested>
+                </C.list_content_reward_wrap>
+              </C.list_content_wrap_inner>
+            </C.list_content_wrap>
+          </C.list_wrap>
+        </C.wrap>
       </Layout>
     </>
   );
