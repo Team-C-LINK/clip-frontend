270c4333018c85d1f7d8358e9ad49b19a475b384
jsl0149 <jsl0149@naver.com>
2024-04-08 18:03:30 +0900

    fix : 로그인 페이지 img -> next/image로 수정

--- clip/src/app/login/page.tsx
+++ clip/src/app/login/page.tsx
@@ -7,6 +7,8 @@ import naver from '../../../public/image/NaverLogin.svg';
 import GTS from '../../../public/image/GTS.svg';
 import ClipPhoneImage from '../../../public/image/ClipPhoneImage.svg';
 import triangle from './assets/image/tip.svg';
+import Image from 'next/image';
+
 const Login = () => {
   const kakaoLogin = () => {
     window.location.href = `${process.env.NEXT_PUBLIC_SERVER_HOST}/oauth/kakao`;
@@ -24,15 +26,17 @@ const Login = () => {
           </C.LoginTextFrame>
           <C.ButtonFrame>
             <C.PointNoticeFrame>
-              <img src={giftBox.src}></img>
+              <Image src={giftBox.src} alt="giftbox" width={21} height={21} />
               <C.PointNoticeText>회원 가입만 해도 300P 지급</C.PointNoticeText>
               <C.triangle_tip src={triangle.src}></C.triangle_tip>
             </C.PointNoticeFrame>
             <C.KaKaoButton onClick={kakaoLogin}>
-              <img src={kakao.src}></img>카카오톡으로 계속하기
+              <Image src={kakao.src} alt="kakao" width={32} height={32} />
+              카카오톡으로 계속하기
             </C.KaKaoButton>
             <C.NaverButton>
-              <img src={naver.src}></img>네이버로 계속하기
+              <Image src={naver.src} alt="kakao" width={32} height={32} />
+              네이버로 계속하기
             </C.NaverButton>
             <C.ReturnHomeButton>클립 홈으로 돌아가기</C.ReturnHomeButton>
           </C.ButtonFrame>
@@ -44,7 +48,8 @@ const Login = () => {
               <strong>베타테스트</strong> 이용 가능!
             </C.BannerTopText>
             <C.BannerBottomText>
-              채널 추가 바로가기 <img src={GTS.src}></img>
+              채널 추가 바로가기
+              <Image src={GTS.src} alt="kakao" width={16} height={16} />
             </C.BannerBottomText>
           </C.BannerTextFrame>
           <C.BannerImage src={ClipPhoneImage.src}></C.BannerImage>
