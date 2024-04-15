d1bd95dedc4fef3414292d15d71dd42c4444298c
jsl0149 <jsl0149@naver.com>
2024-04-16 01:26:05 +0900

    feat : 취소형 헤더 routing 추가

--- clip/src/app/SharedComponent/Header/HeaderCancel/HeaderCancel.tsx
+++ clip/src/app/SharedComponent/Header/HeaderCancel/HeaderCancel.tsx
@@ -2,9 +2,9 @@ import * as C from './HeaderCancel.style';
 import cancel from '../../asset/image/cancel.svg';
 import Image from 'next/image';
 
-const HeaderCancel = () => {
+const HeaderCancel = ({route}:{route:string}) => {
   const routing = () => {
-    window.location.href = '';
+    window.location.href = route;
   };
 
   return (
