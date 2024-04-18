874495d40c424daa47a6389011a6337ca4d3ab2a
jsl0149 <jsl0149@naver.com>
2024-04-18 19:58:31 +0900

    feat : jwt를 헤더에 담아서 보내도록 axios 헤더 설정

--- clip/src/app/api/api.tsx
+++ clip/src/app/api/api.tsx
@@ -4,6 +4,7 @@ const api = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_SERVER_HOST}`,
   headers: {
     'Content-Type': 'application/json',
+    Authorization: 'Bearer ' + localStorage.getItem('accessToken'),
   },
   withCredentials: true,
 });
