2522a21ac7bed9cff0ff2406d73f6bbe52fd096a
jsl0149 <jsl0149@naver.com>
2024-04-19 20:36:43 +0900

    refactor : 서버 데이터가 파싱되기 전 변수 처리 추가

--- clip/src/app/join/auth/utils/phoneNumberFormatter.tsx
+++ clip/src/app/join/auth/utils/phoneNumberFormatter.tsx
@@ -1,24 +1,24 @@
 const phoneNumberFormatter = (value: string) => {
-  const inputPhoneNumber = value.replace(/[^0-9]/g, ''); // 숫자 이외의 문자 제거
+  const inputPhoneNumber = value?.replace(/[^0-9]/g, ''); // 숫자 이외의 문자 제거
   let formattedPhoneNumber = '';
 
-  if (inputPhoneNumber.length < 4) {
+  if (inputPhoneNumber?.length < 4) {
     formattedPhoneNumber = inputPhoneNumber;
-  } else if (inputPhoneNumber.length < 7) {
-    formattedPhoneNumber = `${inputPhoneNumber.slice(
+  } else if (inputPhoneNumber?.length < 7) {
+    formattedPhoneNumber = `${inputPhoneNumber?.slice(
       0,
       3
-    )}-${inputPhoneNumber.slice(3)}`;
-  } else if (inputPhoneNumber.length < 11) {
-    formattedPhoneNumber = `${inputPhoneNumber.slice(
+    )}-${inputPhoneNumber?.slice(3)}`;
+  } else if (inputPhoneNumber?.length < 11) {
+    formattedPhoneNumber = `${inputPhoneNumber?.slice(
       0,
       3
-    )}-${inputPhoneNumber.slice(3, 6)}-${inputPhoneNumber.slice(6)}`;
+    )}-${inputPhoneNumber?.slice(3, 6)}-${inputPhoneNumber?.slice(6)}`;
   } else {
-    formattedPhoneNumber = `${inputPhoneNumber.slice(
+    formattedPhoneNumber = `${inputPhoneNumber?.slice(
       0,
       3
-    )}-${inputPhoneNumber.slice(3, 7)}-${inputPhoneNumber.slice(7, 11)}`;
+    )}-${inputPhoneNumber?.slice(3, 7)}-${inputPhoneNumber?.slice(7, 11)}`;
   }
   return formattedPhoneNumber;
 };
