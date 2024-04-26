a105d42453093c8113c490da91e0b1e9f8526557
jsl0149 <jsl0149@naver.com>
2024-04-26 11:41:58 +0900

    feat : API 요청 인터셉터 설정 변경

--- clip/src/app/api/api.tsx
+++ clip/src/app/api/api.tsx
@@ -8,26 +8,26 @@ const api = axios.create({
   withCredentials: true,
 });
 
-// api.interceptors.request.use((config: any) => {
-//   const token = localStorage.getItem('accessToken');
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
+api.interceptors.request.use((config: any) => {
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
-// api.interceptors.response.use(
-//   (res) => {
-//     return res;
-//   },
-//   (error) => {
-//     const token = localStorage.getItem('accessToken');
-//     if (!token) {
-//       alert('로그인이 필요한 서비스 입니다.');
-//       window.location.href = '/login';
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+api.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (error) => {
+    const token = localStorage.getItem('accessToken');
+    if (!token) {
+      alert('로그인이 필요한 서비스 입니다.');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
