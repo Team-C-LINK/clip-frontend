03a5e526744992c1e99b9bc0b18e6719f3454973
jsl0149 <jsl0149@naver.com>
2024-05-01 16:17:17 +0900

    style : 상세 지원 조건의 어색한 위치 수정

--- clip/src/app/recruit/[POST_ID]/asset/components/Condition/components/ConditionDetail/ConditionDetail.tsx
+++ clip/src/app/recruit/[POST_ID]/asset/components/Condition/components/ConditionDetail/ConditionDetail.tsx
@@ -23,18 +23,18 @@ const Wrap = styled.div`
   height: fit-content;
   background: ${(props) => props.theme.BACKGROUND._01};
   border-radius: 0.2rem;
-  justify-content: center;
+  justify-content: start;
 `;
 
 const Inner = styled.ul`
+  position: relative;
+  left: 2rem;
   display: flex;
   flex-direction: column;
   padding: 0rem 0.8rem;
   gap: 1rem;
-
-  width: 77.5dvw;
+  width: fit-content;
   height: fit-content;
-
   font-family: 'Pretendard';
   ${(props) => props.theme.FONT.Regular.B4};
   letter-spacing: 0.03em;
