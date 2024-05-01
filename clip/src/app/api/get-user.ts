3ca6bfdaa67ac8a18bf2bab66fb67d7ff098bd7f
jsl0149 <jsl0149@naver.com>
2024-05-01 15:44:04 +0900

    chore : 유저 정보를 받아올 때 id가 없으면 로그인 화면으로 이동하게 변경

--- clip/src/app/api/get-user.ts
+++ clip/src/app/api/get-user.ts
@@ -1,8 +1,13 @@
 import api from '@/app/api/api';
 
 const getUser = async () => {
-  const res = await api.get(`/members/${localStorage.getItem('id')}`);
-  return res.data;
+  if (!localStorage.getItem('id')) {
+    alert('로그인이 필요한 서비스 입니다.');
+    window.location.href = '/login';
+  } else {
+    const res = await api.get(`/members/${localStorage.getItem('id')}`);
+    return res.data;
+  }
 };
 
 export default getUser;
