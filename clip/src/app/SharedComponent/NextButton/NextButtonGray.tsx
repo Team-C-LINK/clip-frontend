0774d918b69737898b520e0358b375061173d186
jsl0149 <jsl0149@naver.com>
2024-06-04 01:52:34 +0900

    feat : 지원 완료 버튼 추가

---
+++ clip/src/app/SharedComponent/NextButton/NextButtonGray.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+type button = {
+  $size: string;
+};
+
+const NextButtonGray = styled.div<button>`
+  width: ${(props) => props.$size};
+  height: 4.4rem;
+  display: flex;
+  flex-direction: row;
+  border-radius: 0.4rem;
+  font-family: Pretendard;
+  ${(props) => props.theme.FONT.Medium.B2};
+  justify-content: center;
+  align-items: center;
+  background: ${(props) => props.theme.BACKGROUND._DISABLE};
+  color: ${(props) => props.theme.TEXT._03};
+`;
+
+export default NextButtonGray;
