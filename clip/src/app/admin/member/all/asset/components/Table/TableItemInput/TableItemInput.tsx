aad52f3d22decfadbc2a17472557195a2896fc52
jsl0149 <jsl0149@naver.com>
2024-06-17 16:09:33 +0900

    chore : 디렉토리 분리

---
+++ clip/src/app/admin/member/all/asset/components/Table/TableItemInput/TableItemInput.tsx
@@ -0,0 +1,26 @@
+// import styled from 'styled-components';
+
+// interface TableItemInputProps {
+
+// }
+
+// const TableItemInput = () => {
+
+//   const [value, setValue]
+
+//   return <TableItemInput></TableItemInput>
+
+// }
+
+// const TableItem_input = styled.input.attrs({ type: 'text' })`
+//   padding: 0px;
+//   width: 9.8rem;
+//   height: 4rem;
+//   text-align: center;
+//   background: #f9fafc;
+//   border: 1px solid #d9d9d9;
+//   border-radius: 4px;
+//   font-family: 'Pretendard';
+//   ${(props) => props.theme.FONT.Regular.B3};
+//   outline: none;
+// `;
