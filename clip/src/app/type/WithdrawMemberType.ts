fa5f43497f9ad4bc81295fa5ed490ed19a7828ab
jsl0149 <jsl0149@naver.com>
2024-06-17 16:10:51 +0900

    feat : 회원 관리에 필요한 타입 추가

---
+++ clip/src/app/type/WithdrawMemberType.ts
@@ -0,0 +1,12 @@
+type WithDrawMemberType = {
+  [key: string]: string | number;
+  birthYear: string;
+  reason: string;
+  withdrawDay: string;
+  gender: string;
+  id: number;
+  name: string;
+  number: string;
+};
+
+export default WithDrawMemberType;
