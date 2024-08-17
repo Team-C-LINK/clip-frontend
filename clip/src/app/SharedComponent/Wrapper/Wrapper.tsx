d553f7648cd724e1568e1ef47fad6f755c6a08cc
jsl0149 <jsl0149@naver.com>
2024-08-17 21:19:14 +0900

    feat : 모달 핸들러 추가

--- clip/src/app/SharedComponent/Wrapper/Wrapper.tsx
+++ clip/src/app/SharedComponent/Wrapper/Wrapper.tsx
@@ -2,10 +2,21 @@
 
 import { Wrap } from './Wrapper.style';
 import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
 import GlobalStyle from '@/app/globalstyle';
+import { ModalState } from '@/app/admin/announcement/write/asset/Atoms/ModalState';
 
 const Wrapper = ({ children }: { children: React.ReactNode }) => {
   const [isMobile, setIsMobile] = useState<boolean>(true);
+  const [announcementWriteModalState, setAnnouncementWriteModalState] =
+    useRecoilState(ModalState);
+
+  const handleModalState = (e: React.MouseEvent<HTMLDivElement>) => {
+    for (const key in announcementWriteModalState) {
+      setAnnouncementWriteModalState((prev) => ({ ...prev, [key]: false }));
+    }
+  };
+
   useEffect(() => {
     const _isMobile = /Mobi/i.test(window.navigator.userAgent);
     setIsMobile(true);
@@ -19,7 +30,7 @@ const Wrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       {isClient && (
-        <Wrap>
+        <Wrap onClick={handleModalState}>
           <GlobalStyle $isMobile={isMobile} />
           {children}
         </Wrap>
