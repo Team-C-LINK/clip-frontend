d1d7705b589a5bc6fa3c3e37f5d4243f6f4ece86
jsl0149 <jsl0149@naver.com>
2024-08-20 18:44:49 +0900

    feat : 빈 공간을 클릭하면 모달이 닫히게 수정

--- clip/src/app/SharedComponent/Wrapper/Wrapper.tsx
+++ clip/src/app/SharedComponent/Wrapper/Wrapper.tsx
@@ -4,12 +4,12 @@ import { Wrap } from './Wrapper.style';
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import GlobalStyle from '@/app/globalstyle';
-import { ModalState } from '@/app/admin/announcement/write/asset/Atoms/ModalState';
+import { announcementModalStateWriteModify } from '@/app/Atoms/announcementModalStateWriteModify';
 
 const Wrapper = ({ children }: { children: React.ReactNode }) => {
   const [isMobile, setIsMobile] = useState<boolean>(true);
   const [announcementWriteModalState, setAnnouncementWriteModalState] =
-    useRecoilState(ModalState);
+    useRecoilState(announcementModalStateWriteModify);
 
   const handleModalState = (e: React.MouseEvent<HTMLDivElement>) => {
     for (const key in announcementWriteModalState) {
